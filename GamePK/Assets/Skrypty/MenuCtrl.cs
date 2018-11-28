c81d45db0d7ee5a0bef70c52c42383a5498a4d5e
daniel <gdanielch9@gmail.com>
2018-11-28 19:23:12 +0100

    Menu - start, sound, exit

---
+++ GamePK/Assets/Skrypty/MenuCtrl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//using UnityEngine.Audio;
+
+public class MenuCtrl : MonoBehaviour {
+
+	public void LoadScene(string sceneName){
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Exit(){
+        Debug.Log("Time to quit!");
+        Application.Quit();
+    }
+
+    public void ToggleVolume(){
+        AudioListener.volume = AudioListener.volume == 0.0f ? 1.0f : 0.0f;
+    }
+}
