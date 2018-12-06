029d3ab908cb201502b3b510a246622765cf4175
daniel <gdanielch9@gmail.com>
2018-12-06 19:52:35 +0100

    Great music commit

---
+++ GamePK/Assets/Skrypty/BGPauseSound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGPauseSound : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        BGSound.Instance.gameObject.GetComponent<AudioSource>().Pause();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
