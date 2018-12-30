ff0fdbc35a781d1a0714415fe5d5d126921f2166
daniel <gdanielch9@gmail.com>
2018-12-30 14:07:30 +0100

    start to winter boss

---
+++ GamePK/Assets/Skrypty/WinterBoss.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinterBoss : MonoBehaviour {
+
+    private Rigidbody2D winterBoss;
+    // Use this for initialization
+    void Start () {
+        winterBoss = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        winterBoss.velocity = new Vector2(5, GetComponent<Rigidbody2D>().velocity.y);
+    }
+}
